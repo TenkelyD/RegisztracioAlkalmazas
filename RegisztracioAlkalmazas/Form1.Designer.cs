240f3e19a05ef9848eb164d204e5e373040ea1b6
Petrik Lajos Szg <info@petrik.hu>
2019-10-25 11:46:17 +0200

    hozzáad gomb

--- RegisztracioAlkalmazas/Form1.Designer.cs
+++ RegisztracioAlkalmazas/Form1.Designer.cs
@@ -141,6 +141,7 @@
             this.buttonHozzaad.TabIndex = 11;
             this.buttonHozzaad.Text = "Hozzáad";
             this.buttonHozzaad.UseVisualStyleBackColor = true;
+            this.buttonHozzaad.Click += new System.EventHandler(this.buttonHozzaad_Click);
             // 
             // buttonMentes
             // 
--- RegisztracioAlkalmazas/Form1.cs
+++ RegisztracioAlkalmazas/Form1.cs
@@ -41,5 +41,10 @@ namespace RegisztracioAlkalmazas
             
             txt.Close();
         }
+
+        private void buttonHozzaad_Click(object sender, EventArgs e)
+        {
+            listBoxHobbik.Items.Add(textBoxUjHobbi.Text);
+        }
     }
 }
